9a6ae7a47cdae0c34b87c55657f0fc7555ce8464
amalsabu59 <amalsabu59@gmail.com>
2024-03-30 20:13:55 +0530

    TabIcons added

--- App.js
+++ App.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { View } from 'react-native';
-// You'll need to create these components in your project
 import HomeScreen from './screens/Home';
 import UploadScreen from './screens/Upload';
+import { Entypo } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: '#22272C' }}>
         <Tab.Navigator
-          screenOptions={() => ({
-            tabBarLabelStyle: { display: "none" },
-            headerShown: true,
-            tabBarActiveTintColor: "#28B446", // Color of the active tab
-            tabBarInactiveTintColor: "#D9D9D9", // Color of the inactive tabs
+          screenOptions={({ route }) => ({
+            tabBarLabelStyle: { display: 'none' },
+            headerShown: false,
+            tabBarActiveTintColor: '#28B446',
+            tabBarInactiveTintColor: '#D9D9D9',
             tabBarLabelStyle: {
               fontFamily: 'Inter',
               fontSize: 12,
@@ -28,14 +28,30 @@ export default function App() {
             },
             tabBarStyle: [
               {
-                display: "flex",
-                backgroundColor: "#3C4752",
+                display: 'flex',
+                backgroundColor: '#3C4752',
                 borderTopLeftRadius: 35,
                 borderTopRightRadius: 35,
-                width: "100%",
+                width: '100%',
                 height: 60,
               },
             ],
+            contentContainerStyle: {
+              flex: 1,
+              backgroundColor: '#22272C',
+            },
+            tabBarIcon: ({ color, size }) => {
+              let iconName;
+
+              if (route.name === 'Home') {
+                iconName = 'home'; // Specify the icon name for the Home tab
+              } else if (route.name === 'Upload') {
+                iconName = 'upload'; // Specify the icon name for the Upload tab
+              }
+
+              // Return the Entypo component with the specified icon name, color, and size
+              return <Entypo name={iconName} size={size} color={color} />;
+            },
           })}
         >
           <Tab.Screen name="Home" component={HomeScreen} />
@@ -45,6 +61,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-
-
